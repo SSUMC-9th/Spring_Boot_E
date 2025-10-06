51d9d323a2021f764e470454f3c5d9fce7cf083c
AlphaBs <ksi123456ab@naver.com>
2025-10-06 22:02:14 +0900

    mission/#04 - init spring

---
+++ src/main/java/com/umc_study/mission_server/MissionServerApplication.java
@@ -0,0 +1,13 @@
+package com.umc_study.mission_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionServerApplication.class, args);
+	}
+
+}
