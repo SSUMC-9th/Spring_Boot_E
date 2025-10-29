fdd6178fe164391acbab4dbac1ad80fa60611882
Mac Youngchan <macyoungchan@Macui-MacBookPro.local>
2025-10-29 19:18:07 +0900

    프로젝트 고침

---
+++ src/main/java/com/example/umc9th/domain/member/entity/Member.java
@@ -0,0 +1,38 @@
+package com.example.umc9th.domain.member.entity;
+
+import com.example.umc9th.domain.common.BaseEntity;
+import com.example.umc9th.domain.member.enums.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+    @Column(length = 40)
+    private String address;
+
+    private LocalDate birth;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+
+}
