77b12348ea7a19767adc3bc01d0962f96cc4718c
AlphaBs <ksi123456ab@naver.com>
2025-11-04 22:56:23 +0900

    mission/#6 - fetchJoin 적용, 이미지 leftJoin 으로 같이 받아오기

--- src/main/java/com/umc_study/mission_server/review/repository/ReviewQueryDslImpl.java
+++ src/main/java/com/umc_study/mission_server/review/repository/ReviewQueryDslImpl.java
@@ -1,13 +1,14 @@
 package com.umc_study.mission_server.review.repository;
 
-import com.querydsl.core.BooleanBuilder;
+import static com.umc_study.mission_server.member.entity.QMember.member;
+import static com.umc_study.mission_server.review.entity.QReview.review;
+import static com.umc_study.mission_server.review.entity.QReviewImage.reviewImage;
+import static com.umc_study.mission_server.store.entity.QStore.store;
+
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.umc_study.mission_server.member.entity.QMember;
-import com.umc_study.mission_server.review.entity.QReview;
 import com.umc_study.mission_server.review.entity.Review;
-import com.umc_study.mission_server.store.entity.QStore;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +22,13 @@ public class ReviewQueryDslImpl implements ReviewQueryDsl {
     public List<Review> search(Predicate predicate) {
         JPQLQueryFactory queryFactory = new JPAQueryFactory(em);
 
-
         return queryFactory
-            .selectFrom(QReview.review)
-            .join(QMember.member).on(QMember.member.id.eq(QReview.review.author.id))
-            .join(QStore.store).on(QStore.store.id.eq(QReview.review.store.id))
+            .selectFrom(review)
+            .join(member).on(member.id.eq(review.author.id)).fetchJoin()
+            .join(store).on(store.id.eq(review.store.id)).fetchJoin()
+            .leftJoin(reviewImage).on(reviewImage.review.id.eq(review.id)).fetchJoin()
             .where(predicate)
+            .distinct()
             .fetch();
     }
 }
