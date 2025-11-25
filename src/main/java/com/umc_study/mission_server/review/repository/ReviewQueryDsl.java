0e6627b7d05ec539b663f078d644e8176468d7ad
AlphaBs <ksi123456ab@naver.com>
2025-11-25 23:17:01 +0900

    mission/#9 - 가게 리뷰목록 가져오기

--- src/main/java/com/umc_study/mission_server/review/repository/ReviewQueryDsl.java
+++ src/main/java/com/umc_study/mission_server/review/repository/ReviewQueryDsl.java
@@ -3,7 +3,10 @@ package com.umc_study.mission_server.review.repository;
 import com.umc_study.mission_server.review.domain.Review;
 import com.umc_study.mission_server.review.domain.ReviewSearchQueries;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewQueryDsl {
     List<Review> search(ReviewSearchQueries queries);
+    Page<Review> findAllByStoreId(Long storeId, Pageable pageable);
 }
